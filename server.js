1a3868ecd34dc81e4f22d6e5c195f63fe1e95c76
Joel Rivera <joelmrd@gmail.com>
2021-09-05 15:49:56 -0400

    dev and prod ips are stored in object for easier switching

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const handlebars = require('handlebars');
 const fs         = require('fs');
 const path       = require('path');
 
+const ip = {
+	prod: '143.198.173.209',
+	dev: 'localhost'
+};
+
 const init = async () => {
 
     const server = Hapi.server({
 				port: 80,
-        host: '143.198.173.209',
+        host: ip.dev,
         // tls: {
         //   key:  fs.readFileSync(path.join(__dirname, 'key.pem'), 'utf8' ),
         //   cert: fs.readFileSync(path.join(__dirname, 'server.crt'), 'utf8' )
